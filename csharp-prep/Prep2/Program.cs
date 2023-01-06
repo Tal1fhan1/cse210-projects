5b40520ebdb1361f506d32d52e0c4cb8c2b69e30
Tal1fhan1 <talifhaninetshiheni@gmail.com>
2023-01-07 01:43:31 +0200

    W01 Learning Activity Pt2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string percentage = Console.ReadLine();
+
+        int grade = int.Parse(percentage);
+
+        if (grade >= 90)
+        {
+            string symbol = "A";
+            Console.WriteLine($"Your letter grade is {symbol}");
+        }
+        else if (grade >= 80)
+        {
+            string symbol = "B";
+            Console.WriteLine($"Your letter grade is {symbol}");
+        }
+        else if (grade >= 70)
+        {
+            string symbol = "C";
+            Console.WriteLine($"Your letter grade is {symbol}");
+        }
+        else if (grade > 60)
+        {
+            string symbol = "D";
+            Console.WriteLine($"Your letter grade is {symbol}");
+        }
+        else
+        {
+            string symbol = "F";
+            Console.WriteLine($"Your letter grade is {symbol}");
+        }
+
+        if (grade > 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately you failed the class. Don't give up though! Remember that the road to success is paved with failure. You will succeed next time around.");
+        }
     }
 }
