926bb03b9f2919275e09af1f4264612764574e1d
Tal1fhan1 <talifhaninetshiheni@gmail.com>
2023-02-16 03:19:07 +0200

    Learning Activity 04 Complete

---
+++ prepare/Learning04/MathAssignment.cs
@@ -0,0 +1,40 @@
+public class MathAssignment: Assignment
+{
+    private string _textbookSection;
+    private string _problems;
+
+    public MathAssignment()
+    {
+    }
+
+    public MathAssignment(string studentName, string topic, string textbookSection, string problems): base(studentName, topic)
+    {
+        _textbookSection = textbookSection;
+        _problems = problems;
+    }
+
+    public string GetTextbookSection()
+    {
+        return _textbookSection;
+    }
+
+    public void SetTextbookSection(string textbookSection)
+    {
+        _textbookSection = textbookSection;
+    }
+
+    public string GetProblems()
+    {
+        return _problems;
+    }
+
+    public void SetProblems(string problems)
+    {
+        _problems = problems;
+    }
+
+    public string GetHomeworkList()
+    {
+        return $"{GetStudentName()} - {GetTopic()}\n{_textbookSection} {_problems}";
+    }
+}
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment math = new MathAssignment("Roberto Rodriguez", "Fractions", "Section 7.3", "Problems 8-19" );
+        Console.WriteLine(math.GetHomeworkList());
+        Console.WriteLine();
+
+        WritingAssignment writing = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writing.GetWritingInformation());
     }
 }
